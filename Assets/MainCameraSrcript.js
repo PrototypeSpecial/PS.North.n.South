38343e23ed9c9d9190482f7ba95eddd076aa7e24
ejlee <vane77@naver.com>
2014-07-21 00:27:30 +0900

    슈팅 게임 기본 로직 완료
    유니티 구조 테스트용 샘플 코드
    좌우 이동 및 발사 동작 과 UI 작�

--- Assets/MainCameraSrcript.js
+++ Assets/MainCameraSrcript.js
@@ -1,8 +1,49 @@
 ﻿#pragma strict
 // 20140720 ejlee 커밋 & 푸쉬 테스트
 
-function Start () {
+private var box:GameObject;
+private var moveSpeed:float = 10.0f;
 
+
+function Start () {
+	// 씬에 있는거 
+	box = GameObject.Find("Cube");
+}
+
+// 20140720 ejlee 버튼 
+function OnGUI()
+{
+	// Left
+	if(GUI.RepeatButton(Rect(10,270,50,30),"<"))
+	{
+		if( box.transform.position.x < -10 )
+		{
+			box.transform.position.x = 10;
+		}
+		box.transform.Translate(Vector3.left*moveSpeed*Time.deltaTime);
+	}
+		
+	// Right
+	if(GUI.RepeatButton(Rect(100,270,50,30),">"))
+	{
+		if( box.transform.position.x > 10 )
+		{
+			box.transform.position.x = -10;
+		}	
+		box.transform.Translate(Vector3.right*moveSpeed*Time.deltaTime);
+	}
+	
+	// Fire
+	if(GUI.Button(Rect(170,270,50,30),"^"))
+	{
+		// 동적 생성
+		var sphere : GameObject = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+		sphere.transform.position = box.transform.position;
+		sphere.transform.localScale = Vector3(0.3, 0.3, 0.3);
+		
+		sphere.AddComponent("BulletScript");
+	}	
+	
 }
 
 function Update () {
