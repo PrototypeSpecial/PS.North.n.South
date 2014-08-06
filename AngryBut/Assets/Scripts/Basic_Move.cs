2a2a4cc35c05573dadbc11ab796860d547074d19
ejlee <vane77@naver.com>
2014-08-06 23:43:24 +0900

    + Onply AngryBut Resource Pkg

---
+++ AngryBut/Assets/Scripts/Basic_Move.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Basic_Move : MonoBehaviour {
+	
+	public int Speed;
+	
+	void Update () {
+			
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+               transform.Translate (Vector3.left * Speed * Time.deltaTime);
+          	} 		  
+     	if (Input.GetKey (KeyCode.RightArrow)) {
+               transform.Translate (Vector3.right * Speed * Time.deltaTime);
+          	}
+		if (Input.GetKey (KeyCode.UpArrow)) {
+               transform.Translate (Vector3.forward * Speed * Time.deltaTime);
+          	}
+		if (Input.GetKey (KeyCode.DownArrow)) {
+               transform.Translate (Vector3.back * Speed * Time.deltaTime);
+          	}
+		
+	}
+}
---
+++ AngryBut/Assets/Scripts/Move.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections; 
+
+public class Move : MonoBehaviour
+{
+	public float MoveSpeed;
+	Vector3 lookDirection;
+		
+	void Update ()
+	{
+		if (Input.GetKey (KeyCode.LeftArrow)  ||
+		    Input.GetKey (KeyCode.RightArrow) ||
+		    Input.GetKey (KeyCode.UpArrow)    ||
+		    Input.GetKey (KeyCode.DownArrow)) 
+		{
+			float xx = Input.GetAxisRaw ("Vertical");
+			float zz = Input.GetAxisRaw ("Horizontal");	
+			lookDirection = xx * Vector3.forward + zz * Vector3.right;		
+			
+			this.transform.rotation = Quaternion.LookRotation (lookDirection);
+			this.transform.Translate (Vector3.forward * MoveSpeed * Time.deltaTime);	
+		}
+	}
+	
+}
